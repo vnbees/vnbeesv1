45c6ed09bef0a116a581ef86b6c3d431ff023b7d
jackhuynh <quang.huynh@propzy.com>
2017-09-28 16:23:03 +0700

    update request

--- app/Http/Controllers/requestController.php
+++ app/Http/Controllers/requestController.php
@@ -6,11 +6,13 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 class requestController extends BaseController
 {
-    public function getIndex()
-    {
-    	return "RequestController";
+    public function getIndex(Request $request){
+    	$url = $request->input('url');
+    	$token = $request->input('token');
+    	return "url: ".$url."<br/> token: ".$token;
     }
 }
