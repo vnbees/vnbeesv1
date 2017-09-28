cfa055fe2c33c4babfd9a60a1607fe2b86e01906
jackhuynh <quang.huynh@propzy.com>
2017-09-28 16:45:42 +0700

    update request tracking modal

--- app/Http/Controllers/requestController.php
+++ app/Http/Controllers/requestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Tracking;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -11,6 +12,11 @@ use Illuminate\Http\Request;
 class requestController extends BaseController
 {
     public function getIndex(Request $request){
+    	$tracking = new Tracking;
+        $tracking->url = $request->input('url');
+        $tracking->userId = $request->input('token');
+        $tracking->save();
+
     	$url = $request->input('url');
     	$token = $request->input('token');
     	return "url: ".$url."<br/> token: ".$token;
