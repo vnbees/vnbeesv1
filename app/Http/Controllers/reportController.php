33586e13a7e96bfbf4d34186afbda4f8bf200c32
jackhuynh <quang.huynh@propzy.com>
2017-10-02 18:26:28 +0700

    update report

--- app/Http/Controllers/reportController.php
+++ app/Http/Controllers/reportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Tracking;
+use DB;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -12,7 +13,7 @@ use Illuminate\Http\Request;
 class reportController extends BaseController
 {
 	public function getIndex(){
-        return Tracking::all()->toJson();
+        return Tracking::all()->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d')"))->toJson();
 
 	}
 }
