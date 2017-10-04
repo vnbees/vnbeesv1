5d2542bfa5cf08f8dd5f4623b647a1957cae2020
jackhuynh <quang.huynh@propzy.com>
2017-10-04 10:44:03 +0700

    update report filter and simple timeline

--- app/Http/Controllers/reportController.php
+++ app/Http/Controllers/reportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use App\Tracking;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
@@ -12,8 +13,21 @@ use Illuminate\Http\Request;
 class reportController extends BaseController
 {
 	public function getIndex(){
-        return Tracking::all()->sortByDesc('created_at')->groupBy(function($date) {
-                return \Carbon\Carbon::parse($date->created_at)->format('d-M-y');
-            })->toJson();
+        $respose = DB::select('SELECT COUNT(url) AS traffic,url,userId,created_at,updated_at,id FROM tracking GROUP BY url  ORDER BY `id` DESC');
+        foreach ($respose as $key => $value) {
+        	$respose[$key]->userCount = Tracking::where('url',$value->url)->groupBy('userId')->count();
+        }
+        return json_encode($respose);
+        // return Tracking::all()->sortByDesc('created_at')->groupBy(function($date) {
+        //         return \Carbon\Carbon::parse($date->created_at)->format('d-M-y');
+        //     })->toJson();
+	}
+
+	public function getTest(){
+		$respose = DB::select('SELECT COUNT(url) AS traffic,url,userId,created_at,updated_at,id FROM tracking GROUP BY url  ORDER BY `id` DESC');
+		foreach ($respose as $key => $value) {
+			$respose[$key]->userCount = Tracking::where('url',$value->url)->groupBy('userId')->count();
+		}
+		return json_encode($respose);
 	}
 }
