004474915f6cb20813581c214a3cbfa6615e38dd
jackhuynh <quang.huynh@propzy.com>
2017-10-20 12:13:07 +0700

    seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,10 +13,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-                    'name' => 'jack',
-                    'email' => 'jack.huynh.1201@gmail.com',
-                    'password' => Hash::make('123abcABCD'),
-                    'website' => 'http://localhost/wp/'
+                    'name' => 'ni',
+                    'email' => 'ninguyen1510@gmail.com',
+                    'password' => Hash::make('diemthu1510'),
+                    'website' => 'http://lamme.blog'
                 ]);
     }
 }
